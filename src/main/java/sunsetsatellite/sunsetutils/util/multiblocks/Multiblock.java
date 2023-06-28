3ccf2eef97ee35c79d66537a4a6bbbd5ae89800b
MartinSVK12 <martinmilesich2882@gmail.com>
2023-06-28 19:40:31 +0200

    Added method to get all tile entities of a multiblock.

--- src/main/java/sunsetsatellite/sunsetutils/util/multiblocks/Multiblock.java
+++ src/main/java/sunsetsatellite/sunsetutils/util/multiblocks/Multiblock.java
@@ -1,5 +1,6 @@
 package sunsetsatellite.sunsetutils.util.multiblocks;
 
+import net.minecraft.src.Block;
 import net.minecraft.src.NBTTagCompound;
 import net.minecraft.src.World;
 import sunsetsatellite.sunsetutils.SunsetUtils;
@@ -7,6 +8,7 @@ import sunsetsatellite.sunsetutils.util.BlockInstance;
 import sunsetsatellite.sunsetutils.util.Direction;
 import sunsetsatellite.sunsetutils.util.Vec3i;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Multiblock extends Structure{
@@ -23,16 +25,34 @@ public class Multiblock extends Structure{
         this.translateKey = "multiblock."+modId+"."+translateKey+".name";
     }
 
+    public ArrayList<BlockInstance> getTileEntities(World world, BlockInstance origin, Direction dir){
+        ArrayList<BlockInstance> tiles = new ArrayList<>();
+        for (Object o : data.getCompoundTag("Data").func_28110_c()) {
+            NBTTagCompound block = (NBTTagCompound) o;
+            int x = block.getInteger("x");
+            int y = block.getInteger("y");
+            int z = block.getInteger("z");
+            Vec3i pos = new Vec3i(x,y,z).rotate(origin.pos,dir);
+            boolean isTile = block.getBoolean("tile");
+            if(isTile){
+                if(world.getBlockTileEntity(pos.x,pos.y,pos.z) != null){
+                    BlockInstance blockInstance = new BlockInstance(Block.blocksList[world.getBlockId(pos.x,pos.y,pos.z)],pos,world.getBlockMetadata(pos.x, pos.y, pos.z),world.getBlockTileEntity(pos.x, pos.y, pos.z));
+                    tiles.add(blockInstance);
+                }
+            }
+        }
+        return tiles;
+    }
+
     public boolean isValidAt(World world, BlockInstance origin, Direction dir){
         for (Object o : data.getCompoundTag("Data").func_28110_c()) {
-            Vec3i pos;
             NBTTagCompound block = (NBTTagCompound) o;
             int id = getBlockId(block);
             int meta = block.getInteger("meta");
             int x = block.getInteger("x");
             int y = block.getInteger("y");
             int z = block.getInteger("z");
-            pos = new Vec3i(x,y,z).rotate(origin.pos,dir);
+            Vec3i pos = new Vec3i(x,y,z).rotate(origin.pos,dir);
 
             if (world.getBlockId(pos.x, pos.y, pos.z) != id || (world.getBlockId(pos.x, pos.y, pos.z) == id
                     && (world.getBlockMetadata(pos.x, pos.y, pos.z) != meta || meta == -1)
